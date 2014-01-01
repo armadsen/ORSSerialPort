1a0bb44c7544fe28d475941c17ac148bd4167f90
Andrew Madsen <andrew@openreelsoftware.com>
2014-01-01 10:32:18 -0700

    Cocoa demo project now uses Objective-C modules. Requires Xcode 5 to build.

--- Examples/CocoaDemo/Sources/main.m
+++ Examples/CocoaDemo/Sources/main.m
@@ -24,7 +24,7 @@
 //	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <Cocoa/Cocoa.h>
+@import Cocoa;
 
 int main(int argc, char *argv[])
 {
