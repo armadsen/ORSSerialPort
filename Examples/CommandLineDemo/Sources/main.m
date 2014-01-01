5dd1521c19f955bd144dff3f995a583c854112ba
Andrew Madsen <andrew@openreelsoftware.com>
2014-01-01 10:41:02 -0700

    Command line demo now uses Objective-C modules (requires Xcode 5).

--- Examples/CommandLineDemo/Sources/main.m
+++ Examples/CommandLineDemo/Sources/main.m
@@ -24,7 +24,7 @@
 //	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 #import "ORSSerialPort.h"
 #import "ORSSerialPortManager.h"
 
