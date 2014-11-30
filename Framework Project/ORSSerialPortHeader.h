4d32ab468cab7dbf8034cc2bf8c3a6701433756a
Andrew Madsen <andrew@openreelsoftware.com>
2014-11-30 13:16:06 -0700

    Added project to build ORSSerialPort.framework.

---
+++ Framework Project/ORSSerialPortHeader.h
@@ -0,0 +1,11 @@
+//
+//  ORSSerialPortHeader.h
+//  ORSSerialPort
+//
+//  Created by Andrew Madsen on 11/30/14.
+//  Copyright (c) 2014 Open Reel Software. All rights reserved.
+//
+
+#import <ORSSerialPort/ORSSerialPort.h>
+#import <ORSSerialPort/ORSSerialPortManager.h>
+#import <ORSSerialPort/ORSSerialRequest.h>
