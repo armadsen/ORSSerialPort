51f3d47983af97993adf67fa094ec475a8128007
Andrew Madsen <andrew@mixedinkey.com>
2015-03-14 09:50:23 -0600

    Issue #43: Created project for Objective-C Request/Response demo.

---
+++ Examples/RequestResponseDemo/Objective-C/Sources/ORSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ORSAppDelegate.h
+//  RequestResponseDemo
+//
+//  Created by Andrew Madsen on 3/14/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ORSAppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Examples/RequestResponseDemo/Objective-C/Sources/ORSAppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  ORSAppDelegate.m
+//  RequestResponseDemo
+//
+//  Created by Andrew Madsen on 3/14/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import "ORSAppDelegate.h"
+
+@interface ORSAppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+
+@end
+
+@implementation ORSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
