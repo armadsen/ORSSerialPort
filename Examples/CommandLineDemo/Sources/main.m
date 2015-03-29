5cf0b1e07b40e2d6dd7f2b92ae4c1c03df7d2e5c
Andrew Madsen <andrew@openreelsoftware.com>
2015-03-29 09:54:23 -0600

    CommandLineDemo now builds without warnings on 10.7.

--- Examples/CommandLineDemo/Sources/main.m
+++ Examples/CommandLineDemo/Sources/main.m
@@ -24,7 +24,7 @@
 //	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 #import "ORSSerialPort.h"
 #import "ORSSerialPortManager.h"
 
