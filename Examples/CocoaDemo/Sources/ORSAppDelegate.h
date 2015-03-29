a38d513abde1b29600bfea0458ac56cdf44127e3
Andrew Madsen <andrew@openreelsoftware.com>
2015-03-29 10:11:11 -0600

    CocoaDemo now builds without warnings on 10.7 in Xcode 4.6.3.

--- Examples/CocoaDemo/Sources/ORSAppDelegate.h
+++ Examples/CocoaDemo/Sources/ORSAppDelegate.h
@@ -24,7 +24,7 @@
 //	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-@import Cocoa;
+#import <Cocoa/Cocoa.h>
 
 @interface ORSAppDelegate : NSObject <NSApplicationDelegate>
 
--- Examples/CocoaDemo/Sources/ORSSerialPortDemoController.h
+++ Examples/CocoaDemo/Sources/ORSSerialPortDemoController.h
@@ -24,7 +24,7 @@
 //	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-@import Foundation;
+#import <Cocoa/Cocoa.h>
 #import "ORSSerialPort.h"
 
 @class ORSSerialPortManager;
