9bcc03a5a514dc84825b2d52df5282a9860ebfba
Andrew Madsen <andrew@openreelsoftware.com>
2019-06-13 22:39:06 -0600

    Issue #94: Rename SwiftPM target to 'ORSSerial' for backward compatibility

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ORSSerialPort",
+            name: "ORSSerial",
 			path: "Source",
 			exclude: ["ORSSerialBuffer.h"],
 			publicHeadersPath: "."
