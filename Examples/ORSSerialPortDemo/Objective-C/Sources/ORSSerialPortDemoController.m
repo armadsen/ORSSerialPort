3d0fda55db4510e6d216cbdf8b191a499a222938
Andrew Madsen <andrew@openreelsoftware.com>
2022-03-06 14:25:05 -0700

    Fix ORSSerialPortDemo ObjC build by including ORSSerial.framework as a subproject

--- Examples/ORSSerialPortDemo/Objective-C/Sources/ORSSerialPortDemoController.m
+++ Examples/ORSSerialPortDemo/Objective-C/Sources/ORSSerialPortDemoController.m
@@ -25,7 +25,7 @@
 //	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "ORSSerialPortDemoController.h"
-#import "ORSSerialPortManager.h"
+#import <ORSSerial/ORSSerial.h>
 
 @implementation ORSSerialPortDemoController
 
